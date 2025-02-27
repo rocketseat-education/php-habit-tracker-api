31ee66f171c1a6a717d8eded0f762f2651960510
ojuliocesar <juliocontact15@gmail.com>
2025-02-27 13:11:35 -0300

    Entendendo relacionamentos

--- app/Models/Habit.php
+++ app/Models/Habit.php
@@ -6,9 +6,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+use App\Models\User;
 
 class Habit extends Model
 {
     /** @use HasFactory<\Database\Factories\HabitFactory> */
     use HasFactory;
+
+    public function logs(): HasMany
+    {
+
+        return $this->hasMany(HabitLog::class);
+
+    }
+
+    public function user(): BelongsTo
+    {
+
+        return $this->belongsTo(User::class);
+
+    }
 }
--- app/Models/HabitLog.php
+++ app/Models/HabitLog.php
@@ -7,8 +7,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
 class HabitLog extends Model
 {
     /** @use HasFactory<\Database\Factories\HabitLogFactory> */
     use HasFactory;
+
+    public function habit(): BelongsTo
+    {
+
+        return $this->belongsTo(Habit::class);
+
+    }
 }
