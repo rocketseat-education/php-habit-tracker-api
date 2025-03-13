70a37bedf85bea4e72b84f6855bd15095adabd36
ojuliocesar <juliocontact15@gmail.com>
2025-03-12 21:24:20 -0300

    Registrando um novo usuário

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\RegisterRequest;
+use App\Http\Requests\LoginRequest;
 
 use Illuminate\Support\Facades\Hash;
 
 use App\Models\User;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
@@ -25,6 +27,29 @@ class AuthController extends Controller
         return response()->json([
             'token' => $user->createToken('auth_token')->plainTextToken
         ]);
+
+    }
+
+    public function login(LoginRequest $r)
+    {
+
+        $user = User::firstWhere('email', $r->string('email'));
+
+        if (!$user || !Hash::check($r->string('password'), $user->password)) {
+
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrent.']
+            ]);
+
+        }
+
+        $user->tokens()->delete();
+
+        return [
+
+            'token' => $user->createToken('auth_token')->plainTextToken
+
+        ];
 
     }
 
--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,6 @@ Route::middleware('guest')->group(function() {
 
     Route::post('/api/register', [AuthController::class, 'register']);
 
+    Route::post('/api/login', [AuthController::class, 'login']);
+
 });
